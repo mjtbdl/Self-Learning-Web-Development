61d81f08fed4889ae01e24a2d9e1a55fd508da7e
Mecit Bedel <mjt.bdl@gmail.com>
2023-05-04 23:10:23 +0300

    new

--- Worksheets/js worksheet/worksheet.js
+++ Worksheets/js worksheet/worksheet.js
@@ -360,3 +360,38 @@ function yell(msg){
         console.log(e)
     }
 }//try catch example in action.
+
+
+const numbers2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+numbers2.forEach(function(num){
+    if (num%2===0){
+        //console.log(num)
+    }
+})//forEach example. in this example we are printing even numbers.
+
+for (let num of numbers2){
+    //console.log(num)
+}//forEach example. 
+
+const movies = [
+    { title: 'Amadeus', score: 99 },
+    { title: 'Stand By Me', score: 85 },
+    { title: 'Parasite', score: 95 },
+    { title: 'Alien', score: 90 }
+]
+movies.forEach(function(movie){
+    //console.log(`${movie.title} - ${movie.score}/100`)
+})//forEach example. in this example we are printing movies and their scores.
+
+const doubles = numbers2.map(function(num){
+    return num*2;
+})//map example. in this example we are multiplying every number with 2.
+
+const titles = movies.map(function(movie){
+    return movie.title.toUpperCase();
+})//map example. in this example we are making every title uppercase.
+
+const add2 = (x,y) => {
+    return x+y;
+}//arrow function example. in this example we are adding two numbers.
