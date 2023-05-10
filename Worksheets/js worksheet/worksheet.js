d7c7dabeae17c3a6d147fbc784a6567c4d9cd317
Mecit Bedel <mjt.bdl@gmail.com>
2023-05-10 14:43:02 +0300

    Update worksheet.js

--- Worksheets/js worksheet/worksheet.js
+++ Worksheets/js worksheet/worksheet.js
@@ -438,3 +438,10 @@ const prices = [9.99,1.50,19.99,49.99,30.50];
 const total = prices.reduce((total,price) => {
     return total+price
 })//reduce example. in this example we are adding all the prices.
+
+const minPrice = prices.reduce((min,price) => {
+    if(price<min){
+        return price
+    }
+    return min
+})//reduce example. in this example we are finding the minimum price.
