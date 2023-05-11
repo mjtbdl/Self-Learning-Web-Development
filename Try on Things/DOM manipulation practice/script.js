3f03146c15390f4292fa3741fbcaed3f27771d7b
Mecit Bedel <mjt.bdl@gmail.com>
2023-05-11 15:45:21 +0300

    DOM

--- Try on Things/DOM manipulation practice/script.js
+++ Try on Things/DOM manipulation practice/script.js
@@ -23,3 +23,26 @@ function changeTextColorClass() {
 }// this is for changing the text color of all the elements with class "changeable" using getElementsByClassName.
 
 //getElementsByClassName is used to get all the elements with the specified class name.
+
+
+
+
+// we can do all of them using querySelector and querySelectorAll. Here is how:
+
+function changeTextColorQuerySelector() {
+    document.querySelector("#change-me-too").style.color = "red";
+}// this is for changing the text color of the paragraph with id "change-me" using querySelector.
+
+function changeBgColorQuerySelector() {
+    var paragraphs = document.querySelectorAll("li");
+    for (var i = 0; i < paragraphs.length; i++) {
+        paragraphs[i].style.backgroundColor = "#f2f2f2";
+    }
+}// this is for changing the background color of all the paragraphs using querySelectorAll.
+
+function changeTextColorClassQuerySelector() {
+    var elements = document.querySelectorAll(".changeable-too");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.color = "green";
+    }
+}// this is for changing the text color of all the elements with class "changeable" using querySelectorAll.
